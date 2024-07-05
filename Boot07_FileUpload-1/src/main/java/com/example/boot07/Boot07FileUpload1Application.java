820eed4190c225c69f3783bd228276a3f7f659bd
yujun <did124@naver.com>
2024-07-05 17:56:46 +0900

    24.07.05 스프링부트

---
+++ Boot07_FileUpload-1/src/main/java/com/example/boot07/Boot07FileUpload1Application.java
@@ -0,0 +1,19 @@
+package com.example.boot07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+/*
+ * 	@PropertySource (value=" 커스텀 properties 파일의 위치" )
+ * 	classpath: 는 resources 폴더를 가리킨다.
+ */
+@PropertySource(value="classpath:custom.properties")
+@SpringBootApplication
+public class Boot07FileUpload1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot07FileUpload1Application.class, args);
+	}
+
+}
